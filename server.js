f500064fed4e8b295ae298e2eb50039cf8338daa
Fgryspee <frederic.gryspeerdt@student.howest.be>
2015-11-22 12:28:19 +0100

    add stylus

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
-var express = require('express');
+var express = require('express'),
+    stylus = require('stylus'),
+    logger = require('morgan'), //logger binnen express
+    bodyParser = require('body-parser'); //middleware
 
 //bepalen of we in productie of development fase mode
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express(); //aanmaken express applicatie
 
+function compile(str, path){
+    return stylus(str).set('filename', path);
+}
+
 //configuratie express
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(stylus.middleware(
+    {
+        src: __dirname + '/public',
+        compile: compile
+    }
+));
+app.use(express.static(__dirname + '/public')); //static route handling
+
 /*
 * de applicatie moet de index pagina tonen wnn een request wordt gemaakt waarvoor nog
 * geen bestaand path bestaat (404 -> toont index.html)
